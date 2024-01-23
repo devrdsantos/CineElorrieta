ae1dd4e5eafc4777cd455ad48951acbe40f2bb70
devrdsantos <santoss.rdaniel@gmail.com>
2024-01-23 20:38:10 +0100

    [ADD] Java project reto 03 FIRST COMMIT

---
+++ Reto3/src/vista/VentanaPrincipal.java
@@ -0,0 +1,10 @@
+package vista;
+
+public class VentanaPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Reto3/src/vista/VistaDePrueba.java
@@ -0,0 +1,10 @@
+package vista;
+
+public class VistaDePrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

2c14b6cfba5ffc0845e1c12f844d94894d838f5d
Karina M <karinamonetti@gmail.com>
2024-02-07 21:08:51 +0100

    [ADD] BTN Finalizar en PanelSeleccionCine

--- Reto3/src/paneles/PanelSeleccionCine.java
+++ Reto3/src/paneles/PanelSeleccionCine.java
@@ -85,7 +85,28 @@ public class PanelSeleccionCine extends JPanel {
 		btnPrincipal.setForeground(Color.decode("#FFFFFF"));
 		btnPrincipal.setBorderPainted(false);
 		btnPrincipal.setBackground(Color.decode("#C67ACE"));
-		btnPrincipal.setBounds(490, 489, 150, 39);
+		btnPrincipal.setBounds(421, 489, 150, 39);
 		add(btnPrincipal);
+		
+		// BTN SECUNDARIO - FINALIZAR 
+        JButton btnFinalizar = new JButton("Finalizar");
+        btnFinalizar.setOpaque(true);
+        btnFinalizar.setForeground(Color.WHITE);
+        btnFinalizar.setFont(new Font("Verdana", Font.BOLD, 16));
+        btnFinalizar.setContentAreaFilled(true);
+        btnFinalizar.setBorderPainted(false);
+        btnFinalizar.setBackground(new Color(134, 167, 255));
+        btnFinalizar.setBounds(594, 489, 140, 39);
+        
+        // Si selecciona FINALIZAR se cierra el programa
+        btnFinalizar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                
+                System.exit(0);
+
+            }
+        });
+        add(btnFinalizar);
 	}
 }
